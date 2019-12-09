d0c6a160cce43d4abbd41cd884094511d97da51b
VJavierAR <47366939+VJavierAR@users.noreply.github.com>
2019-12-09 04:11:10 -0600

    dsada

--- report/views/views.py
+++ report/views/views.py
@@ -3,7 +3,7 @@
 from odoo import models, fields, api
 import logging, ast
 _logger = logging.getLogger(__name__)
-
+import datetime
 
 class report(models.AbstractModel):
     _name = 'report.report_custom_template'
